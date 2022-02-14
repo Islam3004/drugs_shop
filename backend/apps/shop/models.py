527767c4371234f6c1ab51c24f6622965ecebab6
Sharshenov Islam <islambulut@mail.ru>
2022-02-14 16:41:49 +0600

    just commit

--- backend/apps/shop/models.py
+++ backend/apps/shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField("Название", max_length = 50, unique=True)
-    slug = models.SlugField(max_length = 50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
 
     class Meta:
         verbose_name = "Категория"
