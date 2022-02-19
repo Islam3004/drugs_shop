5aff015acc9aa8d26f19e1fc6a8b16aaeafd183b
Sharshenov Islam <islambulut@mail.ru>
2022-02-19 20:15:40 +0600

    sorted products_list

--- backend/apps/shop/urls.py
+++ backend/apps/shop/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='index_url'),
     path('products/', ProductListView.as_view(), name='products_list_url'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail_url'),
+    path('category_list/<str:category_slug>/', ProductListView.as_view(), name="category_list")
 ]
--- backend/apps/shop/views.py
+++ backend/apps/shop/views.py
@@ -4,6 +4,8 @@ from .models import Products, Reviews, Categories
 from .forms import ReviewsForm
 # Create your views here.
 from .models import Products
+from django.shortcuts import get_object_or_404
+
 
 
 class HomeView(ListView):
@@ -11,11 +13,23 @@ class HomeView(ListView):
     queryset = Products.objects.filter(status=True)
 
 
+
 class ProductListView(ListView):
     template_name = 'store.html'
-    queryset = Products.objects.filter(status=True)
+    model = Products
     context_object_name = 'products'
     paginate_by = 10
+
+    def get_queryset(self, **kwargs):
+        category_slug = self.kwargs.get("category_slug")
+        if category_slug is not None:
+            category = get_object_or_404(Categories, slug=category_slug)
+            queryset = self.model.objects.filter(status=True, category=category)
+            return queryset
+        queryset = self.model.objects.filter(status=True)
+        return queryset
+
+
 
 
 class ProductDetailView(DetailView):
@@ -23,13 +37,14 @@ class ProductDetailView(DetailView):
     model = Products
     context_object_name = 'product'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(ProductDetailView, self).get_context_data(**kwargs)
-    #     related_product = Products.object.filter(status=True, category=self.object.category)
-    #     context['related_product'] = related_product
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        related_product = Products.objects.filter(status=True, category=self.object.category)
+        context['related_product'] = related_product[:4] if len(related_product) > 4 else related_product
+        return context
 
-    
+
+
     
 
 
