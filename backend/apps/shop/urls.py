7b3bcc271cac49a6e94642ac398f5d6ac2b5eb3e
Sharshenov Islam <islambulut@mail.ru>
2022-04-07 22:54:48 +0600

    changing filter and htl,

--- backend/apps/shop/urls.py
+++ backend/apps/shop/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index_url'),
-    path('products/', ProductsListView.as_view(), name='products_list'),
-    path('filter/', ProductListView.as_view(), name='products_list_url'),
+    path('products/', ProductsListView.as_view(), name='products_list_url'),
+    path('filter/', ProductListView.as_view(), name='products_list'),
     path('product/<slug:slug>/', product_detail, name='product_detail_url'),
     path('product/favorites/<int:id>/', add_favorites, name='add_favorites'),
     path('category_list/<str:category_slug>/', ProductcategoryListView.as_view(), name="category_list"),
